f4cfcad8f7301b3af26c85ee845cc82eb5085a2b
amyk8463 <amit.udiwal@paymate.co.in>
2024-06-13 19:18:53 +0530

    Initial commit

---
+++ Api Doc/src/app/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff5722", // change this to your primary color
+    },
+    secondary: {
+      main: "#ff4081", // change this to your secondary color
+    },
+  },
+});
+
+export default theme;
